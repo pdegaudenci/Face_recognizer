8e7ecdc81fabd212bf88e3a65f0f341998e05708
Alexandra <mendozamalasqueza@gmail.com>
2023-01-26 13:14:42 +0100

    función para la pantalla principal con tk

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ import cv2
 from matplotlib import pyplot
 from mtcnn.mtcnn import MTCNN
 import numpy as np
+
+# ------------------------- Funcion de nuestra pantalla principal ------------------------------------------------
+
+def pantalla_principal():
+    global pantalla  # Globalizamos la variable para usarla en otras funciones
+    pantalla = Tk()
+    pantalla.geometry("300x250")  # Asignamos el tamaño de la ventana
+    pantalla.title("Aprende e Ingenia")  # Asignamos el titulo de la pantalla
+    Label(text="Login Inteligente", bg="gray", width="300", height="2",
+          font=("Verdana", 13)).pack()  # Asignamos caracteristicas de la ventana
+
+    # ------------------------- Vamos a Crear los Botones ------------------------------------------------------
+
+    Label(text="").pack()  # Creamos el espacio entre el titulo y el primer boton
+    Button(text="Iniciar Sesion", height="2", width="30", command=login).pack()
+    Label(text="").pack()  # Creamos el espacio entre el primer boton y el segundo boton
+    Button(text="Registro", height="2", width="30", command=registro).pack()
+
+    pantalla.mainloop()
+
+
+pantalla_principal()
