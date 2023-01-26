bc0b5269dffe9734999ac2685736fdaf5c7f8c34
Anghi Sanchez <anghisp3@outlook.es>
2023-01-26 15:02:08 +0100

    funcion que se encargara de registrar el usuario

--- main.py
+++ main.py
@@ -6,28 +6,40 @@ import cv2
 from matplotlib import pyplot
 from mtcnn.mtcnn import MTCNN
 import numpy as np
+# ------------------------Funcion que asignaremos al boton login -------------------------------------------------
 
-# ------------------------- Funcion de nuestra pantalla principal ------------------------------------------------
+def login():
+    global pantalla2
+    global verificacion_usuario
+    global verificacion_contra
+    global usuario_entrada2
+    global contra_entrada2
 
-def pantalla_principal():
-    global pantalla  # Globalizamos la variable para usarla en otras funciones
-    pantalla = Tk()
-    pantalla.geometry("300x250")  # Asignamos el tamaño de la ventana
-    pantalla.title("Aprende e Ingenia")  # Asignamos el titulo de la pantalla
-    Label(text="Login Inteligente", bg="gray", width="300", height="2",
-          font=("Verdana", 13)).pack()  # Asignamos caracteristicas de la ventana
+    pantalla2 = Toplevel(pantalla)
+    pantalla2.title("Login")
+    pantalla2.geometry("300x250")  # Creamos la ventana
+    Label(pantalla2, text="Login facial: debe de asignar un usuario:").pack()
+    Label(pantalla2, text="Login tradicional: debe asignar usuario y contraseña:").pack()
+    Label(pantalla2, text="").pack()  # Dejamos un poco de espacio
 
-    # ------------------------- Vamos a Crear los Botones ------------------------------------------------------
+    verificacion_usuario = StringVar()
+    verificacion_contra = StringVar()
 
-    Label(text="").pack()  # Creamos el espacio entre el titulo y el primer boton
-    Button(text="Iniciar Sesion", height="2", width="30", command=login).pack()
-    Label(text="").pack()  # Creamos el espacio entre el primer boton y el segundo boton
-    Button(text="Registro", height="2", width="30", command=registro).pack()
+    # ---------------------------------- Ingresamos los datos --------------------------
+    Label(pantalla2, text="Usuario * ").pack()
+    usuario_entrada2 = Entry(pantalla2, textvariable=verificacion_usuario)
+    usuario_entrada2.pack()
+    Label(pantalla2, text="Contraseña * ").pack()
+    contra_entrada2 = Entry(pantalla2, textvariable=verificacion_contra)
+    contra_entrada2.pack()
+    Label(pantalla2, text="").pack()
+    Button(pantalla2, text="Inicio de Sesion Tradicional", width=20, height=1, command=verificacion_login).pack()
 
-    pantalla.mainloop()
+    # ------------ Vamos a crear el boton para hacer el login facial --------------------
+    Label(pantalla2, text="").pack()
+    Button(pantalla2, text="Inicio de Sesion Facial", width=20, height=1, command=login_facial).pack()
 
 
-pantalla_principal()
 
 
 # ------------------------Crearemos una funcion para asignar al boton registro --------------------------------
@@ -66,29 +78,49 @@ def registro():
     Label(pantalla1, text="").pack()
     Button(pantalla1, text="Registro Facial", width=15, height=1, command=registro_facial).pack()
 
+
+# ------------------------ Crearemos una funcion que se encargara de registrar el usuario ---------------------
+
+def registrar_usuario():
+    usuario_info = usuario.get()  # Obetnemos la informacion alamcenada en usuario
+    contra_info = contra.get()  # Obtenemos la informacion almacenada en contra
+
+    archivo = open(usuario_info, "w")  # Abriremos la informacion en modo escritura
+    archivo.write(usuario_info + "\n")  # escribimos la info
+    archivo.write(contra_info)
+    archivo.close()
+
+    # Limpiaremos los text variable
+    usuario_entrada.delete(0, END)
+    contra_entrada.delete(0, END)
+
+    # Ahora le diremos al usuario que su registro ha sido exitoso
+    Label(pantalla1, text="Registro Convencional Exitoso", fg="green", font=("Calibri", 11)).pack()
+
+
 # --------------------------- Funcion para almacenar el registro facial --------------------------------------
 
 def registro_facial():
      # Vamos a capturar el rostro
      cap = cv2.VideoCapture(0)  # Elegimos la camara con la que vamos a hacer la deteccion
-        while (True):
+     while (True):
             ret, frame = cap.read()  # Leemos el video
             cv2.imshow('Registro Facial', frame)  # Mostramos el video en pantalla
             if cv2.waitKey(1) == 27:  # Cuando oprimamos "Escape" rompe el video
                 break
-        usuario_img = usuario.get()
-        cv2.imwrite(usuario_img + ".jpg",
+            usuario_img = usuario.get()
+            cv2.imwrite(usuario_img + ".jpg",
                     frame)  # Guardamos la ultima caputra del video como imagen y asignamos el nombre del usuario
-        cap.release()  # Cerramos
-        cv2.destroyAllWindows()
+            cap.release()  # Cerramos
+            cv2.destroyAllWindows()
 
-        usuario_entrada.delete(0, END)  # Limpiamos los text variables
-        contra_entrada.delete(0, END)
-        Label(pantalla1, text="Registro Facial Exitoso", fg="green", font=("Calibri", 11)).pack()
+            usuario_entrada.delete(0, END)  # Limpiamos los text variables
+            contra_entrada.delete(0, END)
+            Label(pantalla1, text="Registro Facial Exitoso", fg="green", font=("Calibri", 11)).pack()
 
         # ----------------- Detectamos el rostro y exportamos los pixeles --------------------------
 
-        def reg_rostro(img, lista_resultados):
+def reg_rostro(img, lista_resultados):
             data = pyplot.imread(img)
             for i in range(len(lista_resultados)):
                 x1, y1, ancho, alto = lista_resultados[i]['box']
@@ -102,45 +134,14 @@ def registro_facial():
                 pyplot.imshow(data[y1:y2, x1:x2])
             pyplot.show()
 
-        img = usuario_img + ".jpg"
-        pixeles = pyplot.imread(img)
-        detector = MTCNN()
-        caras = detector.detect_faces(pixeles)
-        reg_rostro(img, caras)
+            img = usuario_img + ".jpg"
+            pixeles = pyplot.imread(img)
+            detector = MTCNN()
+            caras = detector.detect_faces(pixeles)
+            reg_rostro(img, caras)
 
 
-# ------------------------Funcion que asignaremos al boton login -------------------------------------------------
 
-def login():
-    global pantalla2
-    global verificacion_usuario
-    global verificacion_contra
-    global usuario_entrada2
-    global contra_entrada2
-
-    pantalla2 = Toplevel(pantalla)
-    pantalla2.title("Login")
-    pantalla2.geometry("300x250")  # Creamos la ventana
-    Label(pantalla2, text="Login facial: debe de asignar un usuario:").pack()
-    Label(pantalla2, text="Login tradicional: debe asignar usuario y contraseña:").pack()
-    Label(pantalla2, text="").pack()  # Dejamos un poco de espacio
-
-    verificacion_usuario = StringVar()
-    verificacion_contra = StringVar()
-
-    # ---------------------------------- Ingresamos los datos --------------------------
-    Label(pantalla2, text="Usuario * ").pack()
-    usuario_entrada2 = Entry(pantalla2, textvariable=verificacion_usuario)
-    usuario_entrada2.pack()
-    Label(pantalla2, text="Contraseña * ").pack()
-    contra_entrada2 = Entry(pantalla2, textvariable=verificacion_contra)
-    contra_entrada2.pack()
-    Label(pantalla2, text="").pack()
-    Button(pantalla2, text="Inicio de Sesion Tradicional", width=20, height=1, command=verificacion_login).pack()
-
-    # ------------ Vamos a crear el boton para hacer el login facial --------------------
-    Label(pantalla2, text="").pack()
-    Button(pantalla2, text="Inicio de Sesion Facial", width=20, height=1, command=login_facial).pack()
 
 
 # --------------------------Funcion para el Login Facial --------------------------------------------------------
@@ -220,3 +221,24 @@ def login_facial():
         print("Usuario no encontrado")
         Label(pantalla2, text="Usuario no encontrado", fg="red", font=("Calibri", 11)).pack()
 
+# ------------------------- Funcion de nuestra pantalla principal ------------------------------------------------
+
+def pantalla_principal():
+    global pantalla  # Globalizamos la variable para usarla en otras funciones
+    pantalla = Tk()
+    pantalla.geometry("300x250")  # Asignamos el tamaño de la ventana
+    pantalla.title("Aprende e Ingenia")  # Asignamos el titulo de la pantalla
+    Label(text="Login Inteligente", bg="gray", width="300", height="2",
+          font=("Verdana", 13)).pack()  # Asignamos caracteristicas de la ventana
+
+    # ------------------------- Vamos a Crear los Botones ------------------------------------------------------
+
+    Label(text="").pack()  # Creamos el espacio entre el titulo y el primer boton
+    Button(text="Iniciar Sesion", height="2", width="30", command=login).pack()
+    Label(text="").pack()  # Creamos el espacio entre el primer boton y el segundo boton
+    Button(text="Registro", height="2", width="30", command=registro).pack()
+
+    pantalla.mainloop()
+
+
+pantalla_principal()
