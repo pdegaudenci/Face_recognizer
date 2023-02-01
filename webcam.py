86a3fdb719e3808686525ebb50b3c33e659ef862
pdegaudenci@hotmail.com <pdegaudenci@hotmail.com>
2023-02-01 14:24:42 -0800

    modificado generacion de dicccioanrio de referencias

--- webcam.py
+++ webcam.py
@@ -1,14 +1,24 @@
 #from embedding import generarembedding
 import json
 import cv2
-from detector_MTCNN import pipeline_deteccion_webcam
+from services.detector_MTCNN import *
 
 diccionario={}
 with open("./embeddings.json","r") as file:
     diccionario =json.load(file)
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(F'Running on device: {device}')
 
+dict_referencias = crear_diccionario_referencias(
+                    folder_path    = './imagenes_embbeding',
+                    min_face_size  = 40,
+                    min_confidence = 0.9,
+                    device         = device,
+                    verbose        = True
+                  )
 
 pipeline_deteccion_webcam(
-    dic_referencia   = diccionario,
+    dic_referencia   = 
+dict_referencias ,
    threshold_similaridad = 0.4 )
