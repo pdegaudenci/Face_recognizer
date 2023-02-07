a4bf6a072104e2d71bd8aa27055289a1b6a5bd3f
Pedro Sebastián Degaudenci <73450522+pdegaudenci@users.noreply.github.com>
2023-02-07 05:22:59 -0800

    Aactualizacion de paramteros de pipeline deteccion facial

--- services/webcam.py
+++ services/webcam.py
@@ -1,4 +1,4 @@
-#from embedding import generarembedding
+#Importacion de librerias
 import json
 import  torch
 from .detector_MTCNN import pipeline_deteccion_webcam,crear_diccionario_referencias
@@ -9,6 +9,8 @@ from .detector_MTCNN import pipeline_deteccion_webcam,crear_diccionario_referenc
 def iniciar_webcam():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(F'Running on device: {device}')
+
+    # Obtengo diccionario de referencia de embeddings de las imagenes almacenadas en carpeta imagenes
     dict_referencias = crear_diccionario_referencias(
                     folder_path    ='imagenes',
                     min_face_size  = 40,
@@ -20,8 +22,8 @@ def iniciar_webcam():
 #json.dump(dict_referencias,tf)
 
 
-
-     
+    # Inicia proceso de deteccion de caras , abriendo una nueva ventana
     pipeline_deteccion_webcam(
         dic_referencia   = dict_referencias,
+        output_device="Fiesta Factoria F5",
         threshold_similaridad = 0.4 )
