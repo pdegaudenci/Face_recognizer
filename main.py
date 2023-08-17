c58859cc4a57d6fc5ad176242907e9b26d645086
Pedro Sebastian Degaudenci Ferreira <pdegaude@capgemini.com>
2023-08-17 17:11:33 +0200

    primer contedor de backend creado con docker-compose

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from PySide6.QtCore import QFile
 from Gui.VentanaPrincipal1 import Ventana
 #from Gui.VentanaPrincipal import Ui_MainWindow
 #from Vistas import interfaz
+import asyncio
+import uvicorn
 
-
-if __name__ == "__main__":
-
+async def main():
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
     app = QApplication([])
     myWidget = Ventana()
     myWidget.show()
     app.exec()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
 
 
 
